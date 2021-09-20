d28162b17f30340cd535ef0b5e462ca0d14fe033
Filipe Soccol <filipe.soccol@gmail.com>
2021-09-20 15:36:52 -0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img class="logo" alt="Fluid Miners Logo" src="./assets/logo.png">
+  <Home/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Sarpanch:wght@400;700;900&display=swap');
+
+@-webkit-keyframes move-background {
+	from {
+		background-position: 0px 0px
+	}
+	to { 
+		background-position: -600px 0px
+	}
+}
+
+body {
+  background: url('./assets/background.png');
+  -webkit-animation-name: move-background;
+	-webkit-animation-duration: 20s;
+	-webkit-animation-timing-function: linear;
+	-webkit-animation-iteration-count: infinite;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Sarpanch, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #EBEBEB;
   margin-top: 60px;
+}
+
+.logo {
+  width: 250px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h1>Fluid Miners</h1>
+    <p>
+      Create your own Super Token or wrap a Pre-existing token to create a mineable asteroid.
+    </p>
+    <h3>Create your own Super Token</h3>
+    <p>
+      Create your own Super Token or wrap a Pre-existing token to create a mineable asteroid.
+    </p>
+    <h3>Create your Asteroid</h3>
+    <p>
+      Create your own Super Token or wrap a Pre-existing token to create a mineable asteroid.
+    </p>
+    <h3>Powered by</h3>
+    <ul>
+      <li><a href="https://makerdao.io"><img class="powered" src="../assets/dai.png" alt="Dai"/></a></li>
+      <li><a href="https://superfluid.finance"><img class="powered" src="../assets/superfluid.png" alt="Superfluid"/></a></li>
+      <li><a href="https://doiim.com"><img class="powered" src="../assets/doiim.png" alt="Doiim"/></a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-size: 3rem;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.powered {
+  height: 32px;
+}
+</style>
